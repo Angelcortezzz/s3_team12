d068b40f0c4a19cb18068afbd64215ba26fcfa42
AngelC <149465389+Angelcortezzz@users.noreply.github.com>
2024-05-18 20:52:01 +0800

    Add files via upload

---
+++ streamlit_app_12.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+
+@st.cache_resource
+def load_model(filepath):
+    model = tf.keras.models.load_model(filepath)
+    return model
+
+st.title("Predicting Air Quality using LSTM Model")
+
+model = load_model('C:\Users\Angel\Desktop\streamlit\model.h5')
