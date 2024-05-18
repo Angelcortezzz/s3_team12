c1269842bc4c50e85f9b693e5446035161c926cd
AngelC <149465389+Angelcortezzz@users.noreply.github.com>
2024-05-19 01:57:36 +0800

    Update final.py

--- final.py
+++ final.py
@@ -11,20 +11,18 @@ thresholds = {
     "RH": {"good": 40.0, "moderate": 50.0, "usg": 60.0, "unhealthy": 70.0, "very_unhealthy": 80.0, "hazardous": 90.0}
 }
 
-# Check air quality classification
-def classify_air_quality(co, nmhc, benzene, nox, no2, temp, rh):
+# Check air quality prediction
+def predict_air_quality(co, nmhc, benzene, nox, no2, temp, rh):
     categories = ["good", "moderate", "usg", "unhealthy", "very_unhealthy", "hazardous"]
-    worst_category = "good"  # Initialize worst category as "good"
+    predicted_category = "good"  # Initialize predicted category as "good"
     
-    for param in thresholds:
-        if param not in locals():  # Check if parameter exists
-            continue
-        for category in categories:
-            if locals()[param] > thresholds[param][category]:
-                worst_category = category
-                break  # Exit the loop once a worse category is found
+    for param, param_thresholds in thresholds.items():
+        for category, threshold in param_thresholds.items():
+            if locals()[param] > threshold:
+                predicted_category = category
+                break  # Exit the loop once a category is found
     
-    return worst_category
+    return predicted_category
 
 # Input sliders
 st.title("Air Quality Prediction")
@@ -36,7 +34,7 @@ no2 = st.slider("NO2", min_value=0.0, max_value=100.0, step=0.01, format="%.5f")
 temp = st.slider("Temperature", min_value=-20.0, max_value=50.0, step=0.01, format="%.5f")
 rh = st.slider("RH", min_value=0.0, max_value=100.0, step=0.01, format="%.5f")
 
-# Classify air quality
+# Predict air quality
 if st.button("Predict Air Quality"):
-    air_quality_category = classify_air_quality(co, nmhc, benzene, nox, no2, temp, rh)
+    air_quality_category = predict_air_quality(co, nmhc, benzene, nox, no2, temp, rh)
     st.write(f"The air quality is predicted as: {air_quality_category.capitalize()}")
