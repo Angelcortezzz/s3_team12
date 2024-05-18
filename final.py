015e8c6fe5c38c05bce29c2a5f3fa1c2a379a860
AngelC <149465389+Angelcortezzz@users.noreply.github.com>
2024-05-19 01:22:37 +0800

    Update final.py

--- final.py
+++ final.py
@@ -1,29 +1,5 @@
 import streamlit as st
 
-# CSS to style the slider thumb with a cloud image and set the background image for the entire app
-cloud_slider_css = '''
-<style>
-body {
-    background-image: url('https://e7.pngegg.com/pngimages/401/219/png-clipart-blue-cloud-art-cloud-computing-cloud-blue-cloud-thumbnail.png');
-    background-size: cover;
-}
-
-.st-ck[data-baseweb="slider"] > .st-ck-thumb {
-    background-image: url('https://e7.pngegg.com/pngimages/401/219/png-clipart-blue-cloud-art-cloud-computing-cloud-blue-cloud-thumbnail.png');
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-    width: 25px; /* Adjust the width of the cloud image */
-    height: 25px; /* Adjust the height of the cloud image */
-    border: none; /* Remove border */
-    border-radius: 50%; /* Make it circular */
-}
-</style>
-'''
-
-# Apply the CSS styling to the entire app
-st.markdown(cloud_slider_css, unsafe_allow_html=True)
-
 # Title
 st.title("Air Quality")
 
@@ -43,3 +19,16 @@ with col2:
     temp = st.slider("Temperature", min_value=-20.0, max_value=50.0, step=0.01, format="%.5f")
     rh = st.slider("RH", min_value=0.0, max_value=100.0, step=0.01, format="%.5f")
     ah = st.slider("AH", min_value=0.0, max_value=50.0, step=0.01, format="%.5f")
+
+# CSS to style the entire app with the background image
+background_css = '''
+<style>
+body {
+    background-image: url('https://e7.pngegg.com/pngimages/401/219/png-clipart-blue-cloud-art-cloud-computing-cloud-blue-cloud-thumbnail.png');
+    background-size: cover;
+}
+</style>
+'''
+
+# Apply the CSS styling to the entire app
+st.markdown(background_css, unsafe_allow_html=True)
